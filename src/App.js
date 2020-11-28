aacd7dd9cf185a69a09376477fe3681952e2e1cf
ing-arriola <ingeniero.arriola@gmail.com>
2020-11-27 20:02:34 -0600

    Added accordion component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import Accordion from "./Components/Accordion";
+
+const items = [
+  {
+    title: "What is React",
+    content:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, sint.",
+  },
+  {
+    title: "Why use React?",
+    content:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, sint.",
+  },
+  {
+    title: "How to use React?",
+    content:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, sint.",
+  },
+];
 
 function App() {
-  return "Hello there";
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Accordion.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => (
+    <Fragment key={item.name}>
+      <div className="title active">
+        <i className="dropdown icon"></i>
+        {item.title}
+      </div>
+      <div className="content active">
+        <p>{item.content}</p>
+      </div>
+    </Fragment>
+  ));
+  return <div className="ui styled accordion"> {renderedItems} </div>;
+};
+
+export default Accordion;
