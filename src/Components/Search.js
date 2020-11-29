ec48f8a048a912b8571a8cb8ba5697f088ac6141
ing-arriola <ingeniero.arriola@gmail.com>
2020-11-28 21:26:33 -0600

    Search component now is able to get data fromwikipedia and also it has a button to browse to wikipedia

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -3,20 +3,48 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
   useEffect(() => {
     const search = async () => {
-      await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           format: "json",
           list: "search",
           origin: "*",
+
           srsearch: term,
         },
       });
+
+      setResults(data.query.search);
     };
-    search();
+
+    setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
   }, [term]);
+
+  const rederedResults = results.map((result) => (
+    <div className="item" key={result.pageid}>
+      <div className="right floated content">
+        <a
+          href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          target="_blank"
+          rel="noreferrer"
+          className="ui button"
+        >
+          Go
+        </a>
+      </div>
+      <div className="content">
+        <div className="header">{result.title}</div>
+      </div>
+    </div>
+  ));
 
   return (
     <div>
@@ -27,9 +55,11 @@ const Search = () => {
             value={term}
             onChange={(e) => setTerm(e.target.value)}
             className="input"
+            placeholder="Type what you wanna search for"
           />
         </div>
       </div>
+      <div className="ui celled list">{rederedResults}</div>
     </div>
   );
 };
